93d501cc403d5be7ecfea88c68e5c4650d11c80b
Diego <diego_guala@hotmail.com>
2023-06-24 20:06:50 -0400

    Player Controler look direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,12 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     Vector3 direction;
     public float speed;
+    public Camera cam;
+    Vector3 MousePositionWorld;
+    Vector3 LookDirection;
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -17,5 +20,18 @@ public class PlayerController : MonoBehaviour
     {
         direction = new Vector3 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
         transform.position += direction*speed;
+
+        MousePositionWorld = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y,10f));
+
+
+        print(MousePositionWorld);
+        LookDirection = new Vector3(MousePositionWorld.x-transform.position.x,MousePositionWorld.y
+        -transform.position.y, 0f);
+
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, LookDirection);
+
+        // transform.rotation.SetLookRotation(Vector3.forward,new Vector3(MousePositionWorld.x-transform.position.x,MousePositionWorld.y
+        // -transform.position.y,0f));
+
     }
 }
